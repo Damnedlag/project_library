f66c89ab37bd37b58f7a19944d5db7ef5c4777f3
DamnedLag <dem.jan90@gmail.com>
2020-01-14 03:54:17 +0100

    Added a submit form to the page

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ let myLibrary = [];
 const booksTable = document.getElementById("books")
 const createTable = (array) => {
     for (let objects of array) {
-        i = 1;
         let row = booksTable.insertRow(1)
         for(let [key, value] of Object.entries(objects)) {
             let cell = row.insertCell(0);
@@ -20,14 +19,40 @@ class Book {
     this.title = title;
     this.author = author;
     }
-//  readinfo() {if (this.isread) {return "Read"} else {return "Haven't been read yet"}}
-    info() { `${this.title} by ${this.author}, ${this.pages} pages, ${this.readinfo()}` }
+    info() { `${this.title} by ${this.author}, ${this.pages} pages, ${this.isread}` }
+    addTable() {let row = booksTable.insertRow(1)
+            let cell0 = row.insertCell(0);
+            cell0.innerHTML = this.isread;
+            let cell1 = row.insertCell(0)
+            cell1.innerHTML = this.pages
+            let cell2 = row.insertCell(0);
+            cell2.innerHTML = this.title;
+            let cell3 = row.insertCell(0);
+            cell3.innerHTML = this.author
+        }
     }
 
 // do stuff
     function addBookToLibrary(title) {
     myLibrary.push(title)
  }
+
+const addBook = document.getElementById('send')
+
+addBook.addEventListener('click', e = () => {
+    let newAuthor = document.getElementById('authorinput').value;
+    let newTitle = document.getElementById('titleinput').value;
+    let newPages = document.getElementById('pagesinput').value;
+    let newIsread = document.getElementById('isreadinput').value;
+    let newBook = new Book(newTitle, newAuthor, newPages, newIsread);
+    addBookToLibrary(newBook);
+    newBook.addTable();
+    }
+ )
+
+
+
+
 
 const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Not read')
 const martian = new Book('The Martian', 'Andy Weir', 369, "Read")
