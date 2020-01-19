8bac36dae426dff8c3d8c2f4ad69120a4cb57bc7
DamnedLag <dem.jan90@gmail.com>
2020-01-19 14:36:03 +0100

    myLibrary is now saved to localStorage

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ class Book {
     this.author = author;
     titles.push(this.title);
     }
-    info() { `${this.title} by ${this.author}, ${this.pages} pages, ${this.isread}` }
+    info() { return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isread}` }
     addTable() {
         let row = booksTable.insertRow(1);
             row.id = this.title;
@@ -48,29 +48,35 @@ class Book {
                 booksTable.deleteRow(i);
                 rmvBook(this.title)
             })
+            
             statusButton.addEventListener('click', e = () => {
-                this.isread == "Read" ? this.isread = "Not read" : this.isread = "Read";
-                statusButton.textContent = this.isread;
-                this.isread == "Read" ? statuscell.style.backgroundColor = "green" : statuscell.style.backgroundColor = "red"
+                let index = myLibrary.map(function(e) {return e.title }).indexOf(this.title);
+                myLibrary[index].isread === "Read" ? myLibrary[index].isread = "Unread" : myLibrary[index].isread = "Read";
+                populateStorage();
+                statusButton.textContent = myLibrary[index].isread;
+                console.log(this.isread)
+                myLibrary[index].isread == "Read" ? statuscell.style.backgroundColor = "green" : statuscell.style.backgroundColor = "red";
             })
-    
-    
     }}
 
 // ads books to the library
  function addBookToLibrary(title) {
-    myLibrary.push(title)
     title.addTable();
  }
 //removes book from the library
  const rmvBook = (removableTitle) => {
-    console.log(removableTitle)
     let index = myLibrary.map(function(e) {return e.title }).indexOf(removableTitle);
     myLibrary.splice(index,1);
     let index2 = titles.indexOf(removableTitle);
     titles.splice(index2,1);
+    populateStorage();
 }
-
+//updates status
+//  const changeStatus = (updatableTitle) => {
+//     let index = myLibrary.map(function(e) {return e.title }).indexOf(updatableTitle);
+//     myLibrary[index].isread === "Read" ? myLibrary[index].isread = "Unread" : myLibrary[index].isread = "Read";
+//     populateStorage();
+// }
 
  //function to handle input from the form
 const addBook = document.getElementById('send')
@@ -85,15 +91,45 @@ addBook.addEventListener('click', e = () => {
     let newIsread = document.getElementById('isreadinput').value;
     let newBook = new Book(newTitle, newAuthor, newPages, newIsread);
     addBookToLibrary(newBook);
+    myLibrary.push(newBook);
+    populateStorage();
     }
  )
 
+function populateStorage() {
+    if(!localStorage.getItem('myLibrary')) {
+    //books for demo
+    const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Unread')
+    const martian = new Book('The Martian', 'Andy Weir', 369, "Read")
 
+    addBookToLibrary(hobbit);
+    addBookToLibrary(martian);
+    myLibrary.push(hobbit);
+    myLibrary.push(martian);
+    }
+    let myLibrary_serialized = JSON.stringify(myLibrary);
+    localStorage.setItem('myLibrary', myLibrary_serialized);
+}
 
+function setLibrary() {
+    let myLibrary_deserialized = JSON.parse(localStorage.getItem('myLibrary'));
+    myLibrary = myLibrary_deserialized;
+    myLibrary.map(function(e) { let elem = new Book(e.title, e.author, e.pages, e.isread); 
+    addBookToLibrary(elem);
+})
+}
+
+if(!localStorage.getItem('myLibrary')) {
+    populateStorage();
+  }
+  else {
+    console.log('van')
+    setLibrary();
+  }
 
 //books for demo
-const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Not read')
-const martian = new Book('The Martian', 'Andy Weir', 369, "Read")
+// const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Unread')
+// const martian = new Book('The Martian', 'Andy Weir', 369, "Read")
 
-addBookToLibrary(hobbit);
-addBookToLibrary(martian)
+// addBookToLibrary(hobbit);
+// addBookToLibrary(martian)
